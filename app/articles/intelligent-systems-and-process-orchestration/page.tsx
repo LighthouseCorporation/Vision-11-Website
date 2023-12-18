ae9cd3532fa3f3c4b5c45a0ccd8d9af345cc1fc3
briankabuginjau <briannjau08@gmail.com>
2023-12-18 18:37:33 +0300

    Typescript Checking

--- app/articles/intelligent-systems-and-process-orchestration/page.tsx
+++ app/articles/intelligent-systems-and-process-orchestration/page.tsx
@@ -1,6 +1,7 @@
 import articleData from "@/components/Articles/articleData";
 import ArticleDetails from "@/components/ArticleDetails";
 import { Metadata } from "next";
+import { ArticleType } from "@/types/article";
 
 export const metadata: Metadata = {
     title: "The Role of Intelligent Systems in Process Automation | A Vision-11 Analysis",
@@ -9,7 +10,11 @@ export const metadata: Metadata = {
 };
 
 const ArticlePage = () => {
-    const data = articleData.find(item=>item.path === "intelligent-systems-and-process-orchestration" )
+    let data = {} as ArticleType
+    const testData = articleData.find(item=>item.path === "intelligent-systems-and-process-orchestration" )
+    if (testData){
+        data=testData
+    }
     return (
             <ArticleDetails article={data} />
     )
