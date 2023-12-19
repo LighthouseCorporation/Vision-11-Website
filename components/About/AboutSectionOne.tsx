d8c8ef25e13ec48ba1b625ac91e9661450138dd6
briankabuginjau <briannjau08@gmail.com>
2023-12-19 12:24:09 +0300

    Removing Aria Description

--- components/About/AboutSectionOne.tsx
+++ components/About/AboutSectionOne.tsx
@@ -22,9 +22,7 @@ const AboutSectionOne = () => {
         <div className="border-b border-body-color/[.15] pb-16 dark:border-white/[.15] md:pb-20 lg:pb-28">
           <div className="-mx-4 flex flex-wrap items-center">
             <div className="w-full p-4  lg:w-1/2">
-              <h1 className="mb-4 text-3xl font-bold !leading-tight text-black dark:text-white sm:text-4xl md:text-[45px]"
-              aria-description="What does it take to build an intelligent system?"
-              >What does it take? </h1>
+              <h1 className="mb-4 text-3xl font-bold !leading-tight text-black dark:text-white sm:text-4xl md:text-[45px]">What does it take? </h1>
               <h2 className="my-5 py-5 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">1. Process Analysis</h2>
               <div
                 className="wow fadeInUp mb-12 max-w-[570px] lg:mb-0"
